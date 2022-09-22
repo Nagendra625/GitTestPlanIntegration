d395401af290a0a36f3c02fed68945447f524e09
Nagendra625 <113166498+Nagendra625@users.noreply.github.com>
2022-09-22 16:45:01 +0530

    Update app.component.ts

--- create-new-angualr/src/app/app.component.ts
+++ create-new-angualr/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'create-new-angualr2 for Github ....';
+  title = 'testting the changes made to create-new-angualr2 for Github ....';
 }
